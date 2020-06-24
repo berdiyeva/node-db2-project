8a4f4b57409b1f36dd055589496a85fb1a726bc3
berdiyeva <berdiyevaaltyn@gmail.com>
2020-06-23 19:57:38 -0500

    updated data

--- data/migrations/20200623162835_cars.js
+++ data/migrations/20200623162835_cars.js
@@ -2,14 +2,16 @@
 exports.up = async function (knex) {
 	await knex.schema.createTable("cars", (table) => {
 		//   table.integer("id").notNull().unique().primary()
-		table.increments("id");
+		table.increments("carID");
 		table.text("name").notNull().unique();
 		table.text("model").notNull();
-		table.real("mileage").notNull();
-		table.real("vin").notNull();
+		table.integer("mileage").notNull();
+		table.text("vin").notNull();
 		table.text("trackTransmissionType");
 		table.text("titleStatus");
 	});
 };
 
-exports.down = function (knex) {};
+exports.down = async function (knex) {
+	await knex.schema.dropTableIfExists("cars");
+};
